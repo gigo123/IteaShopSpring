7690cbc9b84bf0e63f980ed46025aab6844b9151
Taras Hopka <gigo123black@gmail.com>
2019-10-11 15:40:27 +0300

    complete get method

--- src/main/java/ua/itea/RegisterController.java
+++ src/main/java/ua/itea/RegisterController.java
@@ -1,16 +1,18 @@
 package ua.itea;
 
 import java.util.regex.Pattern;
-
 import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import dao.DaoFactory;
 import dao.UserDAO;
 import models.User;
+import mySql.MySQLDAOFactory;
 
 @Controller
 @RequestMapping("/register")
@@ -20,12 +22,42 @@ public class RegisterController {
 	private StringBuilder errorText;
 	private User user;
 	private UserDAO uersDAO;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getRegisterForm() {
-		return null;
-		
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpSession session = attr.getRequest().getSession(true); // true == allow create
+		if (createUser) {
+			createUser = false;
+			return new ModelAndView("redirect:/");
+		}
+		ModelAndView model = new ModelAndView("RegisterView");
+		if (error) {
+			error = false;
+			model.addObject("login", false);
+			model.addObject("errorText", errorText.toString());
+			if (session.getAttribute("login") != null) {
+				model.addObject("login", true);
+			}
+		} else if (session.getAttribute("login") != null) {
+			DaoFactory df = new MySQLDAOFactory();
+			uersDAO = df.getUserDAO();
+			user = uersDAO.getUserByLogin(session.getAttribute("login").toString());
+			model.addObject("login", true);
+		} else {
+			user = new User();
+			model.addObject("login", false);
+		}
+		if (session.getAttribute("cart_number") != null) {
+			model.addObject("items", session.getAttribute("cart_number"));
+		} else {
+			model.addObject("items", 0);
+		}
+		model.addObject("page", "register");
+		model.addObject("user", user);
+		return model;
 	}
+
 	private boolean convertGenderToBool(String gender) {
 		if (gender.equals("Male")) {
 			return true;
