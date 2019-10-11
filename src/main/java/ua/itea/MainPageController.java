ba1d4b86889c1df1082b546b74a8b76f9c42d3c0
Taras Hopka <gigo123black@gmail.com>
2019-10-11 15:19:56 +0300

    complete get method

--- src/main/java/ua/itea/MainPageController.java
+++ src/main/java/ua/itea/MainPageController.java
@@ -1,15 +1,33 @@
 package ua.itea;
 
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class MainPageController {
 	@RequestMapping(method = RequestMethod.GET)
-	public String returnString() {
-		return "MainView";
+	public ModelAndView returnString() {
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpSession session = attr.getRequest().getSession(true); // true == allow create
+		ModelAndView model = new ModelAndView("MainView");
+		if (session.getAttribute("login") != null) {
+			model.addObject("login", true);
+			model.addObject("userName", session.getAttribute("userName"));
+		}
+		if (session.getAttribute("cart") != null) {
+			if (session.getAttribute("cart_number") != null) {
+				model.addObject("items", session.getAttribute("cart_number"));
+			}
+		} else {
+			model.addObject("items", 0);
+		}
+		return model;
 	}
 
 }
