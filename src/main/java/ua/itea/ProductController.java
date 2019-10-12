7dc9fdad9ca01ae715921774f12258f8932ae173
gigo123 <gigo123black@gmail.com>
2019-10-12 08:55:19 +0300

    new productConroller
    based on product servlet
    do basic get method

---
+++ src/main/java/ua/itea/ProductController.java
@@ -0,0 +1,47 @@
+package ua.itea;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import dao.DaoFactory;
+import dao.ProductDAO;
+import models.Product;
+import mySql.MySQLDAOFactory;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getProductList() {
+		List<Product> products;
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpSession session = attr.getRequest().getSession(true); // true == allow create
+		ModelAndView model = new ModelAndView("ProductsView");
+		DaoFactory df = new MySQLDAOFactory();
+		ProductDAO pd = df.getProductDAO();
+		products = pd.getProductList();
+		if (session.getAttribute("login") != null) {
+			model.addObject("login", true);
+			model.addObject("userName", session.getAttribute("userName"));
+		}
+		if (session.getAttribute("cart") != null) {
+			if (session.getAttribute("cart_number") != null) {
+				model.addObject("items", session.getAttribute("cart_number"));
+			}
+		} else {
+			model.addObject("items", 0);
+		}
+		model.addObject("productList", products);
+		model.addObject("page", "product");
+		return model;
+	}
+
+}
